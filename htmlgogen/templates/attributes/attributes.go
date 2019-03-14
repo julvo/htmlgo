1a0a73c6388130aabc926726957204e55c612b8f
Julian Vossen <julian.vossen@rwth-aachen.de>
2019-03-14 23:13:55 +0000

    Escape all attributes by default

--- htmlgogen/templates/attributes/attributes.go
+++ htmlgogen/templates/attributes/attributes.go
@@ -2,23 +2,47 @@ package attributes
 
 import "strings"
 
-type Attribute string
-
-// Build a slice of type []Attribute for cosmetic purposes
-func Attr(attrs ...Attribute) []Attribute {
-    return attrs
+type Attribute struct {
+    Templ       string
+    Data        interface{}
+    Name        string
 }
 
 // Begin of manually implemented attributes
 
 func Dataset(key, value string) Attribute {
-    return Attribute("data-" + key + `="` + value + `"`)
+    return Attribute{
+        Data: map[string]string{
+            "key":      key,
+            "value":    value,
+        },
+        Templ: `{{define "Dataset"}}data-{{.key}}="{{.value}}"{{end}}`,
+        Name: "Dataset",
+    }
+}
+
+func DatasetRaw(key, value string) Attribute {
+    return Attribute{
+        Data: map[string]string{},
+        Templ: `{{define "Dataset"}}data-`+key+`="`+value+`"{{end}}`,
+        Name: "Dataset",
+    }
 }
 
 // Begin of generated attributes
-
 [[ range .AttributeFuncs ]]
-func [[.FuncName]](values ...string) Attribute {
-    return Attribute(`[[.AttrName]]="` + strings.Join(values, " ") + `"`)
+
+func [[.FuncName]](data interface{}, templs ...string) Attribute {
+    attr := Attribute{ Data: data, Name: "[[.FuncName]]" }
+    if len(templs) == 0 {
+        attr.Templ = `{{define "[[.FuncName]]"}}[[.AttrName]]="{{.}}"{{end}}`
+    } else {
+        attr.Templ = `{{define "[[.FuncName]]"}}[[.AttrName]]="` + strings.Join(templs, " ") + `"{{end}}`
+    }
+    return attr
+}
+
+func [[.FuncName]]Raw(values ...string) Attribute {
+    return [[.FuncName]](nil, values...)
 }
 [[ end ]]
--- htmlgogen/templates/elements.go
+++ htmlgogen/templates/elements.go
@@ -2,6 +2,7 @@ package htmlgo
 
 import (
     "fmt"
+    "io"
     "strings"
     "html"
     "html/template"
@@ -11,17 +12,33 @@ import (
 )
 
 type HTML string
+
 type JS struct {
     templ       string
     data        interface{}
 }
 
-func insertAttributes(attrs []a.Attribute) string {
-    s := ""
-    for _, a := range attrs {
-        s += " " + string(a)
+func WriteTo(w io.Writer, h HTML) {
+    w.Write([]byte(h))
+}
+
+// Build a slice of type []Attribute for cosmetic purposes
+func Attr(attrs ...a.Attribute) []a.Attribute {
+    return attrs
+}
+
+func prepareAttributes(attrs []a.Attribute) (string, string, map[string]interface{}) {
+    data := map[string]interface{}{}
+    templ := ""
+    defs := ""
+
+    for _, attr := range attrs {
+        data[attr.Name] = attr.Data
+        templ += ` {{template "` + attr.Name + `" .` + attr.Name + `}}`
+        defs += attr.Templ
     }
-    return s
+
+    return templ, defs, data
 }
 
 func insertChildren(children ...HTML) string {
@@ -36,21 +53,36 @@ func indent(s, indentation string) string {
     return strings.Replace(s, "\n", "\n" + indentation, -1)
 }
 
+func buildElement(tag string, attrs []a.Attribute, content string, close_ bool) string {
+    templ, defs, data := prepareAttributes(attrs)
+    complTempl := defs + "\n<" + tag + templ + ">" + content
+    if close_ {
+        complTempl += "\n</" + tag +">"
+    }
+
+    t, _ := template.New(tag).Parse(complTempl)
+
+    buf := new(bytes.Buffer)
+    _ = t.Execute(buf, data)
+    return buf.String()
+}
+
 func Element(tag string, attrs []a.Attribute, children ...HTML) HTML {
-    return HTML(
-        "\n<" + tag + insertAttributes(attrs) + ">" +
-        indent(insertChildren(children...), "  ") +
-        "\n</" + tag + ">")
+    return HTML(buildElement(tag, attrs, 
+                indent(insertChildren(children...), "  "), true))
 }
 
 func VoidElement(tag string, attrs []a.Attribute) HTML {
-    return HTML(
-        "\n<" + tag + insertAttributes(attrs) + ">")
+    return HTML(buildElement(tag, attrs, "", false))
 }
 
 // Produce HTML from plain text by escaping
 func Text(v interface{}) HTML {
     return HTML("\n" + html.EscapeString(fmt.Sprint(v)))
+}
+
+func TextRaw(s string) HTML {
+    return HTML(s)
 }
 
 // Begin of manually defined elements
@@ -60,7 +92,7 @@ func Html5(attrs []a.Attribute, children ...HTML) HTML {
 }
 
 func Html5_(children ...HTML) HTML {
-    return Html5(a.Attr(), children...)
+    return Html5(Attr(), children...)
 }
 
 func Doctype(t string) HTML {
@@ -74,37 +106,44 @@ func Script(attrs []a.Attribute, js JS) HTML {
         return Element("script", attrs, HTML("\n" + js.templ))
     }
 
+    complTempl := buildElement("script", attrs,
+                               indent("\n" + js.templ, "  "), true)
+    
     // TODO set verbosity level to enable logging
-    t, err := template.New("_").Parse(
-        "\n<script" + insertAttributes(attrs) + ">" +
-        indent("\n" + js.templ, "  ") + "\n</script>")
+    t, err := template.New("_").Delims("{%$", "$%}").Parse(complTempl)
     if err != nil {
         return Element("script", attrs)
     }
+
     buf := new(bytes.Buffer)
     err = t.Execute(buf, js.data)
     if err != nil {
         return Element("script", attrs)
     }
+
     return HTML(buf.String())
 }
 
 func Script_(js JS) HTML {
-    return Script(a.Attr(), js)
+    return Script(Attr(), js)
 }
 
-func Javascript(data interface{}, templs ...string) JS {
+func JavaScript(data interface{}, templs ...string) JS {
     js := JS{ data: data }
     if len(templs) == 0 {
-        js.templ = "{{.}}"
+        js.templ = "{%$.$%}"
     } else {
-        js.templ = strings.Join(templs, "\n")
+        js.templ = strings.Replace(
+                     strings.Replace(
+                        strings.Join(templs, "\n"),
+                        "{{", "{%$", -1),
+                     "}}", "$%}", -1)
     }
     return js
 }
 
-func Javascript_(templs ...string) JS {
-    return Javascript(nil, templs...)
+func JavaScriptRaw(templs ...string) JS {
+    return JavaScript(nil, templs...)
 }
 
 // Begin of generated elements
@@ -115,7 +154,7 @@ func [[.FuncName]](attrs []a.Attribute, children ...HTML) HTML {
 }
 
 func [[.FuncName]]_(children ...HTML) HTML {
-    return [[.FuncName]](a.Attr(), children...)
+    return [[.FuncName]](Attr(), children...)
 }
 [[ end ]]
 
@@ -126,6 +165,6 @@ func [[.FuncName]](attrs []a.Attribute) HTML {
     return VoidElement("[[.TagName]]", attrs)
 }
 func [[.FuncName]]_() HTML {
-    return [[.FuncName]](a.Attr())
+    return [[.FuncName]](Attr())
 }
 [[ end ]]
